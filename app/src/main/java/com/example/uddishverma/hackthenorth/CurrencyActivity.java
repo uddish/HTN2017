a05c4dd8e9def43ecd1ae6b157b562acd385ad24
uddish <uddishverma22@gmail.com>
2017-09-17 07:45:26 -0400

    Currency converter added

--- app/src/main/java/com/example/uddishverma/hackthenorth/CurrencyActivity.java
+++ app/src/main/java/com/example/uddishverma/hackthenorth/CurrencyActivity.java
@@ -1,13 +1,350 @@
 package com.example.uddishverma.hackthenorth;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.content.res.AppCompatResources;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CurrencyActivity extends AppCompatActivity {
+
+    public static TextView date, currencyTitle, countryTo, countryFrom;
+    public static int flag;
+    public static EditText currency_from;
+    public static TextView currency_to;
+    Typeface tfRegular, tfThin;
+    public static ImageView arrowDown, arrowUp;
+    public static BottomSheetBehavior behavior;
+    RecyclerView recyclerView;
+    CurrencyAdapter mAdapter;
+    ArrayList items = new ArrayList();
+    LinearLayout linearLayoutOne, linearLayoutTwo, mainLayout;
+    public static RelativeLayout relativeOne, relativeTwo;
+    Calendar calendar;
+    int screenHeight = 0;
+    public static EditText search;
+    CardView interchange;
+    public static Context ctx;
+    public static View line;
+
+    String url = "http://138.197.165.209:5000/convert/";
+
+    RequestQueue requestQueue;
+
+    CardView changeCurrency;
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency);
+
+        requestQueue = VolleySingleton.getInstance(this).getRequestQueue();
+
+        initialize();
+
+        ctx = getApplicationContext();
+
+        try {
+            if (!(Prefs.getPrefs("country_from", CurrencyActivity.this).equals("notfound"))) {
+                countryFrom.setText(Prefs.getPrefs("country_from", CurrencyActivity.this));
+            } else {
+                countryFrom.setText("USD");
+            }
+
+            if (!(Prefs.getPrefs("country_to", CurrencyActivity.this).equals("notfound"))) {
+                countryTo.setText(Prefs.getPrefs("country_to", CurrencyActivity.this));
+            } else {
+                countryTo.setText("INR");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        screenHeight = getScreenHeight();
+
+        currencyApi(CurrencyActivity.this);
+
+        Globals.getCountryCode(CurrencyActivity.this);
+
+        //inflating bottom sheet
+        final LinearLayout bottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+        bottomSheet.getLayoutParams().height = getScreenHeight() - 100;
+        behavior = BottomSheetBehavior.from(bottomSheet);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapter = new CurrencyAdapter(CurrencyActivity.this, Globals.NEWcountryCode, Globals.NEWcountriesCurrencies);
+        recyclerView.setAdapter(mAdapter);
+
+        behavior.setPeekHeight(0);
+
+
+        //Setting click listeners on arrows
+        linearLayoutOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = 1;
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+                InputMethodManager imm = (InputMethodManager) CurrencyActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(linearLayoutOne.getWindowToken(), 0);
+//                behavior.setPeekHeight(screenHeight/2);
+            }
+        });
+        linearLayoutTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = 2;
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                InputMethodManager imm = (InputMethodManager) CurrencyActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(linearLayoutTwo.getWindowToken(), 0);
+            }
+        });
+
+        relativeOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                behavior.setPeekHeight(0);
+            }
+        });
+        relativeTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                behavior.setPeekHeight(0);
+            }
+        });
+
+        interchange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                url = "http://138.197.165.209:5000/convert/";
+                String currencyFrom = String.valueOf(countryFrom.getText());
+                String currencyTo = String.valueOf(countryTo.getText());
+                String currencyValue = String.valueOf(currency_from.getText());
+
+                url = url + currencyValue + "/" + currencyFrom + "/" + currencyTo;
+
+                fetchData(requestQueue);
+
+            }
+        });
+
+//        relativeOne.setOnTouchListener(new RelativeLayoutTouchListener(this));
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null) {
+                    mAdapter.filter(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        
+
+        relativeOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager) CurrencyActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(relativeOne.getWindowToken(), 0);
+            }
+        });
+    }
+
+    private void initialize() {
+
+        date = (TextView) findViewById(R.id.date);
+        currencyTitle = (TextView) findViewById(R.id.currency_title);
+        arrowDown = (ImageView) findViewById(R.id.down_arrow);
+        arrowUp = (ImageView) findViewById(R.id.up_arrow);
+        countryTo = (TextView) findViewById(R.id.country_to);
+        countryFrom = (TextView) findViewById(R.id.country_from);
+
+        interchange = (CardView) findViewById(R.id.interchange);
+
+        currency_from = (EditText) findViewById(R.id.currency_from_et);
+        currency_to = (TextView) findViewById(R.id.currency_to_et);
+
+        line = findViewById(R.id.txt_line);
+
+        search = (EditText) findViewById(R.id.search);
+
+        date.setText(getCurrentMonth().substring(0, 3) + " " + getCurrentDate() + "," + getCurrentYear());
+
+        relativeOne = (RelativeLayout) findViewById(R.id.relative_1);
+        relativeTwo = (RelativeLayout) findViewById(R.id.relative_2);
+        linearLayoutOne = (LinearLayout) findViewById(R.id.linearll_1);
+        linearLayoutTwo = (LinearLayout) findViewById(R.id.linearll_2);
+        mainLayout = (LinearLayout) findViewById(R.id.maine_linear_layout);
+
+        tfRegular = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/OpenSans-Regular.ttf");
+        tfRegular = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/OpenSans-Light.ttf");
+        date.setTypeface(tfRegular);
+        currencyTitle.setTypeface(tfRegular);
+        countryTo.setTypeface(tfThin);
+        countryFrom.setTypeface(tfThin);
+
+        hideKeyboard(findViewById(R.id.relative_1));
+        hideKeyboard(findViewById(R.id.relative_2));
+        hideKeyboard(findViewById(R.id.interchange));
+
+        ColorStateList csl = AppCompatResources.getColorStateList(this, R.color.blue);
+        Drawable drawableone = getResources().getDrawable(R.drawable.ic_keyboard_arrow_down);
+        DrawableCompat.setTintList(drawableone, csl);
+        arrowDown.setImageDrawable(drawableone);
+    }
+
+    private int getScreenHeight() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        Log.d(TAG, "getScreenHeight: " + height);
+        return height;
+    }
+
+    public void currencyApi(Activity activity) {
+        //Call api
+    }
+
+    private String getCurrentDate() {
+        calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("dd/MM/yyyy");
+        String strDate = mdformat.format(calendar.getTime());
+        String date = strDate.substring(0, 2);
+        return date;
+    }
+
+    private String getCurrentMonth() {
+        DateFormat dateFormat = new SimpleDateFormat("MM");
+        Date date = new Date();
+        String month = (dateFormat.format(date));
+        String currMonth = null;
+        if (month.equals("01")) {
+            currMonth = "January";
+        } else if (month.equals("02")) {
+            currMonth = "February";
+        } else if (month.equals("03")) {
+            currMonth = "March";
+        } else if (month.equals("04")) {
+            currMonth = "April";
+        } else if (month.equals("05")) {
+            currMonth = "May";
+        } else if (month.equals("06")) {
+            currMonth = "June";
+        } else if (month.equals("07")) {
+            currMonth = "July";
+        } else if (month.equals("08")) {
+            currMonth = "August";
+        } else if (month.equals("09")) {
+            currMonth = "September";
+        } else if (month.equals("10")) {
+            currMonth = "October";
+        } else if (month.equals("11")) {
+            currMonth = "November";
+        } else if (month.equals("12")) {
+            currMonth = "December";
+        }
+        return currMonth;
+    }
+
+    private String getCurrentYear() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("yyyy/MM/dd");
+        String strDate = mdformat.format(calendar.getTime());
+        String year = strDate.substring(0, 4);
+        return year;
+    }
+
+    private void hideKeyboard(View root) {
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    View view = v.getRootView().findFocus();
+                    if (view != null) {
+                        InputMethodManager imm = (InputMethodManager) CurrencyActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    }
+                    return false;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void fetchData(RequestQueue requestQueue) {
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "onResponse: " + response);
+                        try {
+                            currency_to.setText(response.getString("result"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: " + error.toString());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
---
+++ app/src/main/java/com/example/uddishverma/hackthenorth/CurrencyAdapter.java
@@ -0,0 +1,136 @@
+package com.example.uddishverma.hackthenorth;
+
+import android.content.Context;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by uddishverma
+ */
+
+public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.ViewHolder> {
+
+    List<String> countryCode = new ArrayList<>();
+    List<String> countriesCurrencies = new ArrayList<>();
+
+    List<String> copycountryCode = new ArrayList<>();
+    List<String> copycountriesCurrencies = new ArrayList<>();
+
+    Context context;
+
+
+    public static final String TAG = "CurrencyAdapter";
+
+
+    public CurrencyAdapter(Context context, List<String> countryCode, List<String> countriesCurrencies) {
+        this.context = context;
+        this.countriesCurrencies = countriesCurrencies;
+        this.countryCode = countryCode;
+        this.copycountriesCurrencies = countriesCurrencies;
+        this.copycountryCode = countryCode;
+    }
+
+
+    public void filter(String s) {
+        copycountriesCurrencies = new ArrayList<>();
+        copycountryCode = new ArrayList<>();
+        if (s != null && s.length() > 1) {
+            for (int i = 0; i < countriesCurrencies.size(); i++) {
+                if (countriesCurrencies.get(i).toLowerCase().contains(s.toLowerCase())) {
+                    copycountryCode.add(countryCode.get(i));
+                    copycountriesCurrencies.add(countriesCurrencies.get(i));
+                }
+            }
+
+        } else {
+            copycountriesCurrencies = countriesCurrencies;
+            copycountryCode = countryCode;
+        }
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_layout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        if (this.copycountryCode.get(position) != null && this.copycountriesCurrencies.get(position) != null) {
+            if (Globals.getCountryFlag(this.copycountryCode.get(position)) != null) {
+                if (Globals.getCountryFlag(this.copycountryCode.get(position)).length() > 1) {
+                    holder.flag.setText(Globals.getCountryFlag(this.copycountryCode.get(position)));
+                    holder.currency.setText(this.copycountriesCurrencies.get(position));
+                } else {
+                    holder.flag.setVisibility(View.GONE);
+                    holder.currency.setVisibility(View.GONE);
+                }
+            }
+        }
+
+        holder.linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int p = 0;
+                if (CurrencyActivity.flag == 1) {
+                    if (holder.currency.getText().toString().toLowerCase().equals(CurrencyActivity.countryTo.getText().toString().toLowerCase())) {
+                        p = 1;
+                    } else {
+                        CurrencyActivity.countryFrom.setText(holder.currency.getText().toString());
+                    }
+                } else if (CurrencyActivity.flag == 2) {
+                    if (holder.currency.getText().toString().toLowerCase().equals(CurrencyActivity.countryFrom.getText().toString().toLowerCase())) {
+                        p = 1;
+                    } else {
+                        CurrencyActivity.countryTo.setText(holder.currency.getText().toString());
+                    }
+                }
+                if (p == 0) {
+                    CurrencyActivity.behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    CurrencyActivity.behavior.setPeekHeight(0);
+                    CurrencyActivity.currency_to.setText("");
+                    CurrencyActivity.currency_from.setText("");
+                    CurrencyActivity.search.setText("");
+                    InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(holder.linear.getWindowToken(), 0);
+                    Prefs.setPrefs("country_from", CurrencyActivity.countryFrom.getText().toString(), context);
+                    Prefs.setPrefs("country_to", CurrencyActivity.countryTo.getText().toString(), context);
+                } else {
+                    Toast.makeText(context, "Select same currencies on both sides", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return copycountryCode.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView flag, currency;
+        public LinearLayout linear;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            flag = (TextView) itemView.findViewById(R.id.flag);
+            currency = (TextView) itemView.findViewById(R.id.currency);
+            linear = (LinearLayout) itemView.findViewById(R.id.currencylinear);
+        }
+
+    }
+
+
+}
