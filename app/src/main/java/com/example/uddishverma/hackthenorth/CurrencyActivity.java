1968d8cc342ed2ab040a7be0f715a9dfd0cf363c
uddish <uddishverma22@gmail.com>
2017-09-17 07:02:30 -0400

    Hotel done

---
+++ app/src/main/java/com/example/uddishverma/hackthenorth/CurrencyActivity.java
@@ -0,0 +1,13 @@
+package com.example.uddishverma.hackthenorth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency);
+    }
+}
